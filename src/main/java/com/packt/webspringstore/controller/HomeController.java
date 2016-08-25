ef64836d3b3b1ec2d8c2181c05a84fa6715670cc
NemanjaMandic <nemanjans84@gmail.com>
2016-08-25 14:11:44 +0200

    Init commit

---
+++ src/main/java/com/packt/webspringstore/controller/HomeController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.packt.webspringstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String welcome(Model model){
+		model.addAttribute("greeting", "Welcome to Serbia!");
+		model.addAttribute("tagline", "To je sjajno mesto");
+		
+		return "welcome";
+	}
+}
+
